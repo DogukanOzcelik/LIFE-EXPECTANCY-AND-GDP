08608a5c078124c04ca85aeef29d1d187ef7b483
Doğukan Özçelik <dogukanozcelik673@gmail.com>
2025-09-14 00:55:56 +0300

    Life expectancy examined throughout the years

--- script.py
+++ script.py
@@ -5,3 +5,25 @@ import pandas as pd
 
 df = pd.read_csv('all_data.csv')
 print(df.head())
+
+df.rename(columns={"Life expectancy at birth (years)": "life_expentancy"}, inplace=True)
+
+
+########### Has life expectancy increased over time in the six nations? ###########
+# plt.figure(figsize=(10, 8))
+# ax = plt.subplot(2, 1, 1)
+# sns.lineplot(x="Year", y="life_expentancy", data=df)
+
+# print(df.Year.unique())
+# ax.set_xticks(df.Year.unique())
+# plt.xticks(rotation=45)
+
+# plt.subplot(2, 1, 2)
+# sns.boxplot(x="Year", y="life_expentancy", data=df)
+# plt.xticks(rotation=45)
+
+# plt.tight_layout()
+# plt.show()
+# plt.close()
+########### Life expectancy tend to be increasing over time in the six nations. ###########
+
