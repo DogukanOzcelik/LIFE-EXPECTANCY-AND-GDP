9a3ade405753a26887094ce8c79513ef77e0a488
Doğukan Özçelik <dogukanozcelik673@gmail.com>
2025-09-14 01:15:36 +0300

    Average life expectancy for nations

--- script.py
+++ script.py
@@ -30,7 +30,9 @@ df.rename(columns={"Life expectancy at birth (years)": "life_expectancy"}, inpla
 
 
 ########### Has GDP increased over time in the six nations? ###########
+# plt.figure(figsize=(8, 6))
 # sns.lineplot(x='Year', y='GDP', hue='Country', data=df)
+# plt.title("GDP Over the Years")
 # plt.show()
 # plt.close()
 ########### GDP is increased for USA and China but for other countries, it remains constant ###########
@@ -38,7 +40,26 @@ df.rename(columns={"Life expectancy at birth (years)": "life_expectancy"}, inpla
 
 
 ########### Is there a correlation between GDP and life expectancy of a country? ###########
+# plt.figure(figsize=(8, 6))
 # sns.scatterplot(x='life_expectancy', y='GDP', hue='Country', data=df)
+# plt.title("Relation between GDP and Life expectancy")
 # plt.show()
 # plt.close()
 ########### There is a positive correlation for every country except Zimbabwe ###########
+
+
+
+########### What is the average life expectancy in these nations? ###########
+average_life_expectancy = df.groupby('Country').mean('life_expectancy').life_expectancy
+plt.figure(figsize=(8, 6))
+plt.title("Average Life Expectancy")
+sns.barplot(x=average_life_expectancy.index, y=average_life_expectancy.values, palette='husl')
+plt.xticks(rotation=45)
+plt.show()
+plt.close()
+
+########### ###########
+
+
+
+
